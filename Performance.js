f070f1240f0984c383f7c4530e5926e467308548
Zihan231 <Zihanislam231@gmail.com>
2026-05-01 10:50:07 +0600

    new

--- Performance.js
+++ Performance.js
@@ -1,10 +1,18 @@
 
 const start = performance.now();
-
-for (let i = 0; i <= 100; i++){
-    console.log(i)
+let arr1 = [];
+let arr2 = [];
+for (let i = 0; i < 10000; i++){
+    if (i < 5000) {
+        arr1.push(i)
+    }
+    arr2.push(i);
 }
 
 const end = performance.now();
 
-console.log(end - start, "ms");
+// console.log(end - start, "ms");
+
+console.time("find");
+arr1.find(n => n == 100);
+console.timeEnd("find");
