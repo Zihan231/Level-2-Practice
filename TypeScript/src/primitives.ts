306001be3ff28eb07ce29684b95d57fb0932715a
Zihan231 <Zihanislam231@gmail.com>
2026-05-03 23:34:25 +0600

    primitive done

--- TypeScript/src/primitives.ts
+++ TypeScript/src/primitives.ts
@@ -2,4 +2,15 @@
 // string, number, boolean, undefined, null
 
 //ts
-//never, unknown, void  
+//never, unknown, void
+
+let name: string = "Zihan"
+
+let value = "hello";
+
+value = true;
+//ts can auto detect variable type, this is called Type Inference.
+
+//If no type is declared default type is Any.
+
+//type script shows error in development phase while java script in run time.
